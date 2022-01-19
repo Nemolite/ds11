d67036e602076a1be36aab8d398ce61c87f0988c
Nemolite <g25092011@mail.ru>
2022-01-19 08:28:15 +0300

    start

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Дочерняя тема для темы enjoyblog
+ * 
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Helper
+ */
+function show($param){
+	echo "<pre>";
+	print_r($param);
+	echo "</per>";
+}
+
+/**
+* Подключение скриптов и стилей
+*/
+function ds11_scripts_style() {
+
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'ds11-style', get_stylesheet_directory_uri() .'/assets/css/ds11.css' );
+	wp_enqueue_script( 'ds11-script', get_stylesheet_directory_uri() . '/assets/js/ds11.js', array(), $theme_version, true );
+}
+add_action( 'wp_enqueue_scripts', 'ds11_scripts_style' );
+
+/**
+* Возврат раздела виджетов в классический вид
+*/
+add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
+add_filter( 'use_widgets_block_editor', '__return_false' );
+
+?>
